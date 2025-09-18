271e811ad032eee62b43a953125c60216ea9d63e
Paulo-dev-98 <paulobg8@gmail.com>
2025-09-18 19:45:59 -0300

    mudando o filme para fazer teste

--- projeto-java-poo-alura/src/Main.java
+++ projeto-java-poo-alura/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Filme favorito = new Filme();
 
-        favorito.nome = "The Matrix";
-        favorito.anoDeLancamento = 1999;
-        favorito.duracaoEmMinutos = 135;
+        favorito.nome = "The Phantom of the opera";
+        favorito.anoDeLancamento = 2004;
+        favorito.duracaoEmMinutos = 160;
         favorito.incluidoNoPlano = true;
 
         favorito.exibeFichaTecnica();
         favorito.avalia(9);
-        favorito.avalia(8);
+        favorito.avalia(9);
         favorito.avalia(9);
 
         System.out.println("Média de avaliações do filme: " + favorito.pegaMedia());
