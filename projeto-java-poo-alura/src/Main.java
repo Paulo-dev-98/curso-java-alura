8d968f8210084c829196678611e8723c06fdea17
Paulo-dev-98 <paulobg8@gmail.com>
2025-09-18 19:40:48 -0300

    inicio do curso de poo alura

--- projeto-java-poo-alura/src/Main.java
+++ projeto-java-poo-alura/src/Main.java
@@ -2,8 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Filme favorito = new Filme();
 
-        System.out.println("teste para projeto de poo");
+        favorito.nome = "The Matrix";
+        favorito.anoDeLancamento = 1999;
+        favorito.duracaoEmMinutos = 135;
+        favorito.incluidoNoPlano = true;
 
+        favorito.exibeFichaTecnica();
+        favorito.avalia(9);
+        favorito.avalia(8);
+        favorito.avalia(9);
+
+        System.out.println("Média de avaliações do filme: " + favorito.pegaMedia());
     }
 }
